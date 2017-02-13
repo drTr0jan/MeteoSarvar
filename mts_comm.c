dc5affba3392c24b85aa210711f786baf68886ef
drTr0jan <drtr0jan@yandex.ru>
2017-02-13 22:28:06 +1000

    Added the RR loop.

--- mts_comm.c
+++ mts_comm.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include <iconv.h>
 #include <errno.h>
+#include <pthread.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netdb.h> 
 #include <netinet/in.h>
 #include <unistd.h>
 #include <time.h>
+#include <limits.h>
 #include "convert.h"
-#include "filename.h"
+#include "files.h"
+
+#define WORK_DIR  "/var/db/meteo/"
+#define LINK_PATH "LINK/PRM_ASPD_S/"
+#define BCKP_PATH "backup/"
 
 #define	TCP_IP_DATA	1
 #define	TCP_IP_DATA_Z	5
@@ -36,6 +43,9 @@ typedef struct __attribute__((__packed__)) {
   uint8_t pri;
   uint8_t pad[3];
 } SRVPKT;
+
+int sock;
+pthread_mutex_t rr_mutex;
 
 int serv_connect(const char *hostname, uint16_t port)
 {
@@ -73,6 +83,7 @@ int serv_connect(const char *hostname, uint16_t port)
   if (connect(sock, aip->ai_addr, aip->ai_addrlen) < 0)
   {
     close(sock);
+    freeaddrinfo(aip);
     return -1;
 /*    if ((errno == ETIMEDOUT) ||
         (errno == ECONNREFUSED) ||
@@ -86,25 +97,71 @@ int serv_connect(const char *hostname, uint16_t port)
     else
       return ECONO;*/
   }
+  freeaddrinfo(aip);
   return sock;
+}
+
+void *rr_loop(void *arg)
+{
+  const SRVPKT rrmsg = {
+    .typy = TCP_IP_RR,
+    0,
+    0x03,0x8f,0x6a,
+    0,0,
+    "            ",
+    .pri = 2,
+    {0,0,0}
+  };
+  
+  while (1)
+  {
+    sleep(60);
+    pthread_mutex_lock(&rr_mutex);
+    if((write(sock, &rrmsg, sizeof(rrmsg))) > 0)
+      printf("RR has been sent successful.\n");
+    else
+      printf("RR error has been occured.\n");
+    pthread_mutex_unlock(&rr_mutex);
+  }
+  return((void *)0);
 }
 
 int main(int argc, char *argv[])
 {
   iconv_t cd;
+  char rr = 1;
 
   const unsigned short port = 7251;
   const char * hostname = "95.167.117.38";
+  
+//_POSIX_PATH_MAX
+  char msg_path[] = LINK_PATH "00000000.00p";
+  int msg_fname = strlen(msg_path) - 12;
+  
+#ifdef BCKP_PATH
+  char bak_path[] = BCKP_PATH "00000000.00p";
+  int bak_fname = strlen(bak_path) - 12;
+#endif
 
   srandom(time(NULL));
+  umask(0);
+  chdir(WORK_DIR);
+
+/*  get_filename(&bak_path[bak_fname]);
+  printf("Hello: %s\n",bak_path);
+
+  exit(0);*/
+
   cd = iconv_open("CP1251","KOI8-R");
+  if (rr)
+    pthread_mutex_init(&rr_mutex, NULL);
 
   // Main loop
   while (1)
   {
-    int sock, readbytes;
+    int readbytes;
     SRVPKT *spbuf;
-
+    pthread_t tid;
 
     // Connect statement
     if ((sock = serv_connect(hostname,port)) < 0)
@@ -114,8 +171,12 @@ int main(int argc, char *argv[])
       continue;
     }
 
+    // If RR is enabled start the RR loop
+    if(rr)
+      pthread_create(&tid, NULL, rr_loop, NULL);
+
     // Read section
-    spbuf = (SRVPKT *) malloc(sizeof(*spbuf));    
+    spbuf = (SRVPKT *) malloc(sizeof(*spbuf));
     while ((readbytes = read(sock, spbuf, sizeof(*spbuf))) > 0)
     {
 
@@ -124,7 +185,7 @@ int main(int argc, char *argv[])
       {
         int len;
         char filename[13];
-        FILE *fp;
+//        FILE *fp;
         char * msg;
 
         len = ntohs(spbuf->len);
@@ -144,9 +205,11 @@ int main(int argc, char *argv[])
         
         // Save the data
         get_filename(filename);
-        fp = fopen(filename,"w");
+        write_msg(filename,msg,len);
+/*        fp = fopen(filename,"w");
         fwrite(msg,len,1,fp);
-        fclose(fp);
+        fclose(fp);*/
+        
         free(msg);
         printf("Saving success: %s\n", filename);
         
@@ -157,10 +220,14 @@ int main(int argc, char *argv[])
         {
           // Sending an acknowledgement
           spbuf->typy = TCP_IP_ACK;
+          if (rr)
+            pthread_mutex_lock(&rr_mutex);
           if ((write(sock, spbuf, sizeof(*spbuf))) > 0)
             printf("ACK has been sent successful.\n");
           else
             printf("An send error has been occured. Error code is: %d\n",errno);
+          if (rr)
+            pthread_mutex_unlock(&rr_mutex);
         }
         else
           printf("A capturing error has been occured. The packet type is %d, but must be TCP_IP_END.\n",spbuf->typy);
@@ -180,8 +247,12 @@ int main(int argc, char *argv[])
       printf("An receive error has been occured. Error code is: %d\n",errno);
     if (readbytes == 0)
       printf("An EOF has been gotten.\n");
+    
+    if (rr)
+      pthread_cancel(tid);
+    sleep(1);
     close(sock);
-    sleep(5);
+    sleep(3);
   }    
 
   iconv_close(cd);
